e418c96e81896275ee7168d78c321e8f9d87944e
RamonSanGabriel <jeshero11@gmail.com>
2024-12-05 18:27:16 +0800

    update folder

--- react-useContext/src/component/App/App.jsx
+++ react-useContext/src/component/App/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import UserMenu from './component/UserMenu/UserMenu';
+import UserMenu from '../UserMenu/UserMenu';
 
 function App() {
   return (
